d3d77fe24ba986b80cdea984193605f2a1954204
Douglascrc <douglasbuscam@gmail.com>
2025-11-06 12:16:56 -0300

    feat: implement JWT authentication with JwtTokenFilter and JwtUtil classes

---
+++ src/main/java/br/com/codigodebase/helpdesk/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package br.com.codigodebase.helpdesk.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String schemeName = "bearerAuth";
+
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes(schemeName, bearerScheme))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName));
+    }
+}
